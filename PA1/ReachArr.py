6ce077cb01571af4b8d18d827b7af5dc9e3ba8f1
androidwargames <adwagner89@gmail.com>
2016-06-18 14:26:26 -0500

    Created Tree Objects with pre/post order values

---
+++ PA1/ReachArr.py
@@ -0,0 +1,50 @@
+#Uses python3
+
+import sys
+
+
+class Tree:
+    def __init__(self, adj):
+        self.preOrd = [0] * len(adj)
+        self.postOrd = self.preOrd
+        self.found = [False] * len(adj)
+        self.adj = adj
+        self.count = 0
+        self.size = len(adj)
+
+    def explore(self, i):
+        for j in self.adj[i]:
+            if not self.found[j]:
+                self.preOrd[j] = self.count
+                self.count += 1
+                self.found[j] = True
+                self.explore(j)
+                self.postOrd[j] = self.count
+                self.count += 1
+
+    def explored(self):
+        return self.count == self.size
+
+
+def reach(adj, x, y):
+    t = Tree(adj)
+    t.explore(x)
+    if t.found[y]:
+        return 1
+    else:
+        return 0
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    data = list(map(int, input.split()))
+    n, m = data[0:2]
+    data = data[2:]
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+    x, y = data[2 * m:]
+    adj = [[] for _ in range(n)]
+    x, y = x - 1, y - 1
+    for (a, b) in edges:
+        adj[a - 1].append(b - 1)
+        adj[b - 1].append(a - 1)
+    print(reach(adj, x, y))
