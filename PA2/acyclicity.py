f49c7d98f79cfffce3d227e3f17c003b4babecbc
androidwargames <adwagner89@gmail.com>
2016-06-18 15:29:48 -0500

    finished Acyclicity and toposort (first try awwww yeah)

--- PA2/acyclicity.py
+++ PA2/acyclicity.py
@@ -4,7 +4,46 @@ import sys
 
 
 def acyclic(adj):
-    return 0
+    t = Tree(adj)
+    if t.cycle():
+        return 1
+    else:
+        return 0
+
+
+class Tree:
+    def __init__(self, adj):
+        self.preOrd = [-1] * len(adj)
+        self.postOrd = [-1] * len(adj)
+        self.found = [False] * len(adj)
+        self.adj = adj
+        self.count = 0
+        self.size = len(adj)
+        self.cyclic = False
+
+    def explore(self, i):
+        self.found[i] = True
+        self.preOrd[i] = self.count
+        self.count += 1
+        for j in self.adj[i]:
+            if not self.found[j]:
+                self.explore(j)
+            elif self.postOrd[j] == -1:
+                self.cyclic = True
+        self.postOrd[i] = self.count
+        self.count += 1
+
+    def cycle(self):
+        for i in range(self.size):
+            if not self.cyclic:
+                if not self.found[i]:
+                    self.explore(i)
+            else:
+                break
+        return self.cyclic
+
+    def explored(self):
+        return self.count == self.size
 
 if __name__ == '__main__':
     input = sys.stdin.read()
--- PA2/toposort.py
+++ PA2/toposort.py
@@ -2,16 +2,48 @@
 
 import sys
 
-def dfs(adj, used, order, x):
-    #write your code here
-    pass
+class Tree:
+    def __init__(self, adj):
+        self.preOrd = [-1] * len(adj)
+        self.postOrd = [-1] * len(adj)
+        self.found = [False] * len(adj)
+        self.adj = adj
+        self.count = 0
+        self.size = len(adj)
+        self.cyclic = False
+
+    def explore(self, i):
+        self.found[i] = True
+        self.preOrd[i] = self.count
+        self.count += 1
+        for j in self.adj[i]:
+            if not self.found[j]:
+                self.explore(j)
+            elif self.postOrd[j] == -1:
+                self.cyclic = True
+        self.postOrd[i] = self.count
+        self.count += 1
+
+    def cycle(self):
+        for i in range(self.size):
+            if not self.cyclic:
+                if not self.found[i]:
+                    self.explore(i)
+            else:
+                break
+        return self.cyclic
+
+    def explored(self):
+        return self.count == self.size
 
 
 def toposort(adj):
-    used = [0] * len(adj)
-    order = []
-    #write your code here
-    return order
+    t = Tree(adj)
+    for i in range(t.size):
+        if not t.found[i]:
+            t.explore(i)
+    result = sorted(range(t.size), key=lambda k: t.postOrd[k])
+    return result[::-1]
 
 if __name__ == '__main__':
     input = sys.stdin.read()
