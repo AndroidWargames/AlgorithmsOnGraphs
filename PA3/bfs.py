3419fcf4668924299b49a71341276e9c801fb75a
androidwargames <adwagner89@gmail.com>
2016-06-25 16:24:19 -0500

    finished BFS

--- PA3/bfs.py
+++ PA3/bfs.py
@@ -1,11 +1,38 @@
 #Uses python3
 
 import sys
-import queue
+
+class BFSTree:
+    def __init__(self, adj, s):
+        self.size = len(adj)
+        self.dist = [self.size] * self.size
+        self.adj = adj
+        self.queue = [s]
+        self.s = s
+        self.dist[s] = 0
+
+    def explore(self):
+        while len(self.queue) > 0:
+            u = self.dequeue()
+            for i in self.adj[u]:
+                if self.dist[i] == self.size:
+                    self.queue.append(i)
+                    self.dist[i] = self.dist[u] + 1
+
+    def dequeue(self):
+        x = self.queue[0]
+        self.queue.pop(0)
+        return x
+
 
 def distance(adj, s, t):
-    #write your code here
-    return -1
+    tree = BFSTree(adj, s)
+    tree.explore()
+    result = tree.dist[t]
+    if result == tree.size:
+        return -1
+    else:
+        return result
 
 if __name__ == '__main__':
     input = sys.stdin.read()
