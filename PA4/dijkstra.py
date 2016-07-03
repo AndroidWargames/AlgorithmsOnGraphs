43449802c4f347a56e6583e90a6d33f92cb0711a
androidwargames <adwagner89@gmail.com>
2016-07-02 23:37:55 -0500

    Finished Dijkstra and neg cycle

--- PA4/dijkstra.py
+++ PA4/dijkstra.py
@@ -1,12 +1,54 @@
 #Uses python3
 
 import sys
-import queue
+
+
+class DijkstraTree:
+    def __init__(self, adj, cost):
+        self.size = len(adj)
+        self.lim = sum(sum(i) for i in cost) + 1
+        self.dist = [self.lim] * self.size
+        self.adj = adj
+        self.prev = [-1] * self.size
+        self.unknown = [True] * self.size
+        self.cost = cost
+
+    def distance(self, s, g):
+        self.dist[s] = 0
+        self.unknown[s] = False
+        for i in range(self.size):
+            self.relax(s)
+            p = s
+            s = self.extractMin()
+            if s == -1:
+                break
+            self.prev[s] = p
+        if self.dist[g] == self.lim:
+            return -1
+        else:
+            return self.dist[g]
+
+    def extractMin(self):
+        min = self.lim
+        ind = -1
+        for i in range(self.size):
+            if self.unknown[i]:
+                if self.dist[i] < min:
+                    ind = i
+                    min = self.dist[i]
+        self.unknown[ind] = False
+        return ind
+
+    def relax(self, x):
+        dis = self.dist[x]
+        for i in range(len(self.adj[x])):
+            if self.cost[x][i] + dis < self.dist[adj[x][i]]:
+                self.dist[adj[x][i]] = self.cost[x][i] + dis
 
 
 def distance(adj, cost, s, t):
-    #write your code here
-    return -1
+    tree = DijkstraTree(adj, cost)
+    return tree.distance(s, t)
 
 
 if __name__ == '__main__':
--- PA4/negative_cycle.py
+++ PA4/negative_cycle.py
@@ -3,9 +3,41 @@
 import sys
 
 
+class NegCycleTree:
+    def __init__(self, adj, cost):
+        self.size = len(adj)
+        self.edges = sum(len(i) for i in adj)
+        self.dist = [sum(sum(abs(j) for j in i) for i in cost) + 1] * self.size
+        self.adj = adj
+        self.unknown = [True] * self.size
+        self.cost = cost
+        self.cycle = False
+
+    def nc(self):
+        self.dist[0] = 0
+        for i in range(self.size + 1):
+            r = False
+            for j in range(self.size):
+                r = r or self.relax(j)
+            if not r:
+                return 0
+            elif i == self.size:
+                return 1
+
+    def relax(self, x):
+        rel = False
+        dis = self.dist[x]
+        for i in range(len(self.adj[x])):
+            if self.cost[x][i] + dis < self.dist[adj[x][i]]:
+                self.dist[adj[x][i]] = self.cost[x][i] + dis
+                rel = True
+        return rel
+
+
 def negative_cycle(adj, cost):
-    #write your code here
-    return 0
+    t = NegCycleTree(adj, cost)
+    return t.nc()
+
 
 
 if __name__ == '__main__':
