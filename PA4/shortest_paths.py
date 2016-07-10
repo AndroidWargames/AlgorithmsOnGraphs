227e744c6c01201beaf2beb180558cda5ebe3842
androidwargames <adwagner89@gmail.com>
2016-07-10 10:40:58 -0500

    init PA5

--- PA4/shortest_paths.py
+++ PA4/shortest_paths.py
@@ -3,10 +3,40 @@
 import sys
 import queue
 
+class ShortestPathTree:
+    def __init__(self, adj, cost):
+        self.size = len(adj)
+        self.reachable = [0] * self.size
+        self.shortest = [1] * self.size
+        self.lim = sum(sum(abs(j) for j in i) for i in cost) + 1
+        self.dist = [self.lim] * self.size
+        self.adj = adj
+        self.prev = [-1] * self.size
+        self.cost = cost
 
-def shortet_paths(adj, cost, s, distance, reachable, shortest):
-    #write your code here
-    pass
+    def sPath(self, s):
+        self.dist[s] = 0
+        self.reachable[s] = 1
+        for i in range(self.size*2):
+            r = False
+            for j in range(self.size):
+                r = r or self.relax(j, i >= self.size)
+            if not r:
+                break
+
+    def relax(self, x, r2):
+        rel = False
+        dis = self.dist[x]
+        for i in range(len(self.adj[x])):
+            node = adj[x][i]
+            if self.cost[x][i] + dis < self.dist[node]:
+                self.dist[node] = self.cost[x][i] + dis
+                rel = True
+                if r2:
+                    self.shortest[node] = 0
+                else:
+                    self.reachable[node] = 1
+        return rel
 
 
 if __name__ == '__main__':
@@ -23,15 +53,13 @@ if __name__ == '__main__':
         cost[a - 1].append(w)
     s = data[0]
     s -= 1
-    distance = [10**19] * n
-    reachable = [0] * n
-    shortest = [1] * n
-    shortet_paths(adj, cost, s, distance, reachable, shortest)
+    t = ShortestPathTree(adj, cost)
+    t.sPath(s)
     for x in range(n):
-        if reachable[x] == 0:
+        if t.reachable[x] == 0:
             print('*')
-        elif shortest[x] == 0:
+        elif t.shortest[x] == 0:
             print('-')
         else:
-            print(distance[x])
+            print(t.dist[x])
 
